9c66e3c2b0eb90a90ac0c3b78be3472c17ead49f
Lucas <lucasdeshok@gmail.com>
2019-06-14 20:01:45 -0300

    atualizacao projeto

--- PrimeiroProjeto/Models/Cliente.cs
+++ PrimeiroProjeto/Models/Cliente.cs
@@ -10,7 +10,7 @@ namespace PrimeiroProjeto.Models
     public class Cliente
     {
         [Display(Name ="Código")]
-        public int IDCliente { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
         public int Filhos { get; set; }
         [Display(Name = "Endereço")]
